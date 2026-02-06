f731ac13663c909d53ab90983a37169a35270dfb
fvrvz <codefaraaz@gmail.com>
2026-02-06 19:51:51 +0530

    feat: redesign login page

--- src/lib/components/features/auth/login/Login.svelte
+++ src/lib/components/features/auth/login/Login.svelte
@@ -4,7 +4,7 @@
 	import { Auth } from '$lib/resources/auth';
 	import { loginSchema } from '$lib/schemas/auth';
 	import { authStore } from '$lib/state/auth.svelte';
-	import { Button, Input, Label } from 'flowbite-svelte';
+	import { A, Button, Input, Label } from 'flowbite-svelte';
 	import { defaults, superForm } from 'sveltekit-superforms';
 	import { zod4, zod4Client } from 'sveltekit-superforms/adapters';
 
@@ -33,44 +33,37 @@
 	const { form, tainted, enhance, formId } = superform;
 </script>
 
-<div class="m-auto max-w-4xl">
-	<h1 class="mb-10 text-4xl font-extrabold text-gray-900 dark:text-white">
-		Login
-	</h1>
-	<form use:enhance class="flex flex-col flex-wrap bg-gray-50" id={$formId}>
-		<section class=" flex items-stretch max-sm:flex-col">
-			<article
-				class="flex flex-1/2 items-center justify-center bg-blue-900"
-			></article>
+<form
+	use:enhance
+	class="m-auto max-w-lg space-y-4 rounded-lg border border-gray-300 bg-gray-50 p-6"
+	id={$formId}
+>
+	<h1 class="mb-5 text-4xl dark:text-white">Login</h1>
+	<div class="space-y-2">
+		<Label for="username">Username</Label>
+		<Input
+			type="text"
+			bind:value={$form.userId}
+			id="username"
+			placeholder="Enter username or email"
+			required
+		/>
+	</div>
+	<div class="space-y-2">
+		<Label for="password">Password</Label>
+		<Input
+			type="password"
+			bind:value={$form.password}
+			id="password"
+			placeholder="Enter password"
+			required
+		/>
+	</div>
 
-			<div
-				class="flex flex-1/2 flex-col items-center justify-around gap-5 p-5 pl-0"
-			>
-				<div class="shrink grow">
-					<Label for="username">Username</Label>
-					<Input
-						type="text"
-						bind:value={$form.userId}
-						id="username"
-						placeholder="Enter username or email"
-						required
-					/>
-				</div>
-				<div class="shrink grow">
-					<Label for="password">Password</Label>
-					<Input
-						type="password"
-						bind:value={$form.password}
-						id="password"
-						placeholder="Enter password"
-						required
-					/>
-				</div>
-
-				<div class="flex w-full items-center justify-center gap-4">
-					<Button type="submit" form={$formId}>Login</Button>
-				</div>
-			</div>
-		</section>
-	</form>
-</div>
+	<div class="flex items-center justify-between gap-2 max-sm:flex-col">
+		<A class="text-sm sm:text-base" href="/register">Create an account</A>
+		<Button type="submit" form={$formId} class="w-full cursor-pointer sm:w-auto">
+			Login
+		</Button>
+	</div>
+</form>
