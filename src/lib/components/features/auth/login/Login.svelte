c02083db7d457ffb04d769d3b96fc451937d32d1
fvrvz <codefaraaz@gmail.com>
2026-02-06 19:29:07 +0530

    refactor: utilize component library and fix the redirection on login

--- src/lib/components/features/auth/login/Login.svelte
+++ src/lib/components/features/auth/login/Login.svelte
@@ -1,47 +1,44 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { resolve } from '$app/paths';
-	import Button from '$lib/components/common/button/Button.svelte';
-	import Input from '$lib/components/common/input/Input.svelte';
 	import { Auth } from '$lib/resources/auth';
 	import { loginSchema } from '$lib/schemas/auth';
 	import { authStore } from '$lib/state/auth.svelte';
+	import { Button, Input, Label } from 'flowbite-svelte';
 	import { defaults, superForm } from 'sveltekit-superforms';
 	import { zod4, zod4Client } from 'sveltekit-superforms/adapters';
 
-	let isLoading = $state(false);
+	const superform = superForm(
+		defaults({ userId: '', password: '' }, zod4(loginSchema)),
+		{
+			validators: zod4Client(loginSchema),
+			SPA: true,
+			onUpdate: async ({ form }) => {
+				if (!form.valid || !$tainted) return;
 
-	const superform = superForm(defaults(zod4(loginSchema)), {
-		validators: zod4Client(loginSchema),
-		SPA: true,
-		resetForm: false,
-		onUpdate: async ({ form }) => {
-			if (!form.valid || !$tainted) return;
+				const [err, response] = await Auth.login(form.data);
 
-			isLoading = true;
+				if (err) {
+					throw new Error('Something went wrong', err);
+				}
 
-			const [err, response] = await Auth.login(form.data);
+				authStore.setAuth(response);
+			},
+			onUpdated: () => {
+				goto(resolve('/'));
+			},
+		}
+	);
 
-			if (err) {
-				isLoading = false;
-				throw new Error('Something went wrong', err);
-			}
-
-			authStore.setAuth(response);
-			goto(resolve('/'));
-			isLoading = false;
-		},
-	});
-
-	const { form, tainted, enhance } = superform;
+	const { form, tainted, enhance, formId } = superform;
 </script>
 
 <div class="m-auto max-w-4xl">
 	<h1 class="mb-10 text-4xl font-extrabold text-gray-900 dark:text-white">
 		Login
 	</h1>
-	<form use:enhance class="flex flex-col flex-wrap bg-gray-50">
-		<section class=" flex items-stretch">
+	<form use:enhance class="flex flex-col flex-wrap bg-gray-50" id={$formId}>
+		<section class=" flex items-stretch max-sm:flex-col">
 			<article
 				class="flex flex-1/2 items-center justify-center bg-blue-900"
 			></article>
@@ -49,26 +46,29 @@
 			<div
 				class="flex flex-1/2 flex-col items-center justify-around gap-5 p-5 pl-0"
 			>
-				<Input
-					label="Username"
-					type="text"
-					name="username"
-					id="username"
-					bind:value={$form.userId}
-					required
-					class="shrink grow"
-				/>
-				<Input
-					label="Password"
-					type="password"
-					name="password"
-					id="password"
-					bind:value={$form.password}
-					required
-					class="shrink grow"
-				/>
+				<div class="shrink grow">
+					<Label for="username">Username</Label>
+					<Input
+						type="text"
+						bind:value={$form.userId}
+						id="username"
+						placeholder="Enter username or email"
+						required
+					/>
+				</div>
+				<div class="shrink grow">
+					<Label for="password">Password</Label>
+					<Input
+						type="password"
+						bind:value={$form.password}
+						id="password"
+						placeholder="Enter password"
+						required
+					/>
+				</div>
+
 				<div class="flex w-full items-center justify-center gap-4">
-					<Button type="submit">Login</Button>
+					<Button type="submit" form={$formId}>Login</Button>
 				</div>
 			</div>
 		</section>
