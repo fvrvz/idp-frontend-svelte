be1a86fb01cdb79e9f8fd4a0625030d6c2f7ae55
fvrvz <codefaraaz@gmail.com>
2026-02-08 01:49:30 +0530

    chore: add user endpoints

--- src/lib/resources/users.ts
+++ src/lib/resources/users.ts
@@ -1,18 +1,34 @@
 import api from '$lib/api/api';
 import { promiseWrapper, type Result } from '$lib/api/promise-wrapper';
-import type { User } from '$lib/types/auth.type';
 import type { ListModel } from '$lib/types/common..type';
+import type {
+	User,
+	UserRequest,
+	UserUpdateRequest,
+} from '$lib/types/user.type';
 
 export class Users {
-	static register(body: User) {
+	static register(body: UserRequest) {
 		return promiseWrapper(
 			api.post('/users/register', body, { withCredentials: false })
 		);
 	}
 
-	static async get(): Promise<Result<User[]>> {
-		const [err, res] = await promiseWrapper<ListModel<User>>(api.get('/users/'));
+	static async getList(): Promise<Result<User[]>> {
+		const [err, res] = await promiseWrapper<ListModel<User>>(api.get('/users'));
 		if (err) return [err, null];
 		return [null, res.data];
+	}
+
+	static async get(username: string) {
+		return promiseWrapper<User>(api.get(`/users/${username}`));
+	}
+
+	static async delete(username: string) {
+		return promiseWrapper<void>(api.delete(`/users/${username}`));
+	}
+
+	static async update(username: string, body: UserUpdateRequest) {
+		return promiseWrapper<User>(api.patch(`/users/${username}`, body));
 	}
 }
